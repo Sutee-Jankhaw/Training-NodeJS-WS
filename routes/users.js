171243e20b351223bf2d9e0b8d68155af517234a
Mark <sutee.j@igeargeek.com>
2026-09-03 16:48:09 +0700

    add register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var userSchema = require('../models/users.model')
+const bcrypt = require('bcrypt');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/',async function(req, res, next) {
+  let { username, password, role } = req.body
+  let user = new userSchema({
+    username: username,
+    password: await bcrypt.hash(password, 10),
+    status: 'not_approved',
+    role: role
+  })
+  await user.save()
 });
 
 module.exports = router;
